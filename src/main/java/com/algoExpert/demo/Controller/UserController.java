8a83196cdd0f9ce6aafb92b167b0fa7a093a8234
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-03-03 13:49:01 +0200

    update: added 3 endpoints to read,update and delete user by id

--- src/main/java/com/algoExpert/demo/Controller/UserController.java
+++ src/main/java/com/algoExpert/demo/Controller/UserController.java
@@ -3,12 +3,12 @@ package com.algoExpert.demo.Controller;
 import com.algoExpert.demo.Entity.User;
 import com.algoExpert.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -16,13 +16,47 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+//    create and save user
     @PostMapping("/saveUser")
     public User saveUser(@RequestBody User user){
         return userService.create(user);
     }
 
+//    get all user from db
     @GetMapping("/getAllUsers")
     public List<User> getAll(){
         return userService.getUsers();
     }
+
+//    get user by id
+@GetMapping("/user/{user_id}")
+public ResponseEntity<User> getUserById(@PathVariable Integer user_id) {
+    Optional<User> userOptional = userService.findById(user_id);
+
+    if (userOptional.isPresent()) {
+        User user = userOptional.get();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
+    // Update user by id
+	@PutMapping("/update/{user_id}")
+	public User updateUser(@PathVariable Integer user_id, @RequestBody User updatedUser) {
+        return userService.updateUser(user_id, updatedUser);
+	}
+
+//    @PutMapping("/update/{id}")
+//    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User updatedUser) {
+//            userService.updateUser(id, updatedUser);
+//        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+//    }
+
+    // Delete user by id
+    @DeleteMapping("/delete/{user_id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Integer user_id) {
+        userService.deleteUser(user_id);
+        return ResponseEntity.ok("User with ID " + user_id + " was deleted successfully.");
+    }
+
 }
